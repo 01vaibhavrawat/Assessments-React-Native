a490c990569b103f910539a090547e93e3264f43
vaibhavxcitech <119841873+vaibhavxcitech@users.noreply.github.com>
2023-10-23 19:36:43 +0530

    created draft 1 for home page UI in tab 2

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,108 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground, Image } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+const HomePage: React.FC = () => {
+  // Assuming the user's current day is 5, you can replace this with dynamic data
+  const currentDay = 5;
 
-export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <ImageBackground
+      source={require('@/assets/images/procrastination.jpg')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Image source={require('@/assets/images/procrastination.jpg')} style={styles.logo} />
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>30-Day Challenge</Text>
+          <Text style={styles.dayText}>Day {currentDay}</Text>
+        </View>
+
+        <TouchableOpacity style={styles.studyButton}>
+          <Text style={styles.buttonText}>Complete Daily Study</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.quizButton}>
+          <Text style={styles.buttonText}>Complete Daily Quiz</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dayNavigation}>
+          <TouchableOpacity style={styles.navigationButton}>
+            <Text style={styles.navigationButtonText}>Previous Day</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.navigationButton}>
+            <Text style={styles.navigationButtonText}>Next Day</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
   },
-  title: {
-    fontSize: 20,
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 32,
+  },
+  header: {
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 28,
     fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 8,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  dayText: {
+    fontSize: 18,
+    color: 'white',
+    marginBottom: 32,
+  },
+  studyButton: {
+    backgroundColor: '#3498db', // Button color
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  quizButton: {
+    backgroundColor: '#27ae60', // Button color
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  dayNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  navigationButton: {
+    backgroundColor: '#e74c3c', // Navigation button color
+    padding: 16,
+    borderRadius: 8,
+    width: 140,
+  },
+  navigationButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
+
+export default HomePage;
